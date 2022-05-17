9cb92103c7c3ed76015a086d2ef1a2acd9667f2a
Avinash Mekala <55137704+avinash0106@users.noreply.github.com>
2022-05-17 08:26:08 +0530

    Add files via upload
    These pipelines are created to test retry mechanism

---
+++ declarative_retry.groovy
@@ -0,0 +1,25 @@
+pipeline {
+    agent any
+    stages {
+        stage('Fetch') {
+            steps {
+                sh 'git clone https://github.com/linuxacademy/content-pipelines-cje-labs.git'
+            }
+        }
+        stage('Build'){
+            steps{
+                sh 'gcc --std=c99 -o mario content-pipelines-cje-labs/lab1_lab2/mario.c'
+            }
+        }
+    }
+
+    post{
+        failure {
+            timeout(time: 5, unit: 'Minutes') {
+                retry {
+                    ${env.JOB_NAME}.run
+                }
+            }
+        }
+    }
+}
