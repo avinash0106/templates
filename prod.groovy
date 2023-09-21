b7e2fe41fc2ffcb2aed9335058f55040b3b72cdc
Avinash Mekala <55137704+avinash0106@users.noreply.github.com>
2023-09-21 15:37:23 +0530

    Update prod.groovy

--- prod.groovy
+++ prod.groovy
@@ -1,3 +1,4 @@
+
 pipeline {
     agent any
     options {
@@ -5,14 +6,17 @@ pipeline {
     }
     tools {
         terraform 'terraform'
-
+    }
+    environment {
+        AWS_REGION = 'ap-south-1'
+        AWS_ACCOUNT_ID = '702024837763'
     }
     stages {
         stage('Checkout') {
             steps {
                 script {
                     try {
-                        sh 'cp -r /app/osione-infra/* .'
+                        sh 'cp -r /apps/terraform/* .'
                     } catch (Exception e) {
                         currentBuild.result = 'FAILURE'
                         error("Error Copying files ${e.getMessage()}")
@@ -21,13 +25,13 @@ pipeline {
             }
         }
 
-        stage('Terraform Initialise') {
+        stage('Terraform Initialise and Plan') {
             steps {
                 script {
                     try {
                         sh 'terraform init'
                         sh 'terraform validate'
-
+                        sh 'terraform plan -out=tfplan'
                     } catch (Exception e) {
                         currentBuild.result = 'FAILURE'
                         error("Terraform planning failed: ${e.getMessage()}")
@@ -36,26 +40,83 @@ pipeline {
             }
         }
 
-        stage('Terraform Plan/Apply') {
+        stage('Terraform Apply') {
             steps {
-                withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', credentialsId: 'AWS']]) {
-                    script {
-                        try {
-                        // Set AWS CLI credentials using Jenkins credentials
-                        sh 'aws configure set aws_access_key_id \$AWS_ACCESS_KEY_ID'
-                        sh 'aws configure set aws_secret_access_key \$AWS_SECRET_ACCESS_KEY'
-                        sh 'aws configure set region ap-south-1'
+                script {
+                    try {
+                        withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', credentialsId: 'AWS']]) {
+                            // Set AWS CLI credentials using Jenkins credentials
+                            sh 'aws configure set aws_access_key_id \$AWS_ACCESS_KEY_ID'
+                            sh 'aws configure set aws_secret_access_key \$AWS_SECRET_ACCESS_KEY'
+                            sh 'aws configure set region ${AWS_REGION}'
+                            
+                            // Print AWS CLI configuration for verification (optional)
+                            sh 'aws configure list'
+                            
+                            sh 'terraform apply "tfplan" -auto-approve'
+                        }
+                    } catch (Exception e) {
+                        currentBuild.result = 'FAILURE'
+                        error("Terraform apply failed: ${e.getMessage()}")
+                    }
+                }
+            }
+        }
 
-                        // Print AWS CLI configuration for verification (optional)
-                        sh 'aws configure list'
+        stage('ECR Config') {
+            steps {
+                script {
+                    try {
+                        withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', credentialsId: 'AWS']]) {
+                            sh "aws ecr get-login-password --region ${AWS_REGION} | docker login --username AWS --password-stdin ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com"
+                        }
+                    } catch (Exception e) {
+                        currentBuild.result = 'FAILURE'
+                        error("ECR login failed: ${e.getMessage()}")
+                    }
+                }
+            }
+        }
 
-                        sh 'terraform plan -out=tfplan'
-                        // sh 'terraform apply -auto-approve'
-                        } finally {
-                            // Clear AWS CLI credentials after use (optional)
-                            archiveArtifacts allowEmptyArchive: true, artifacts: '**/tfplan', followSymlinks: false
-                            archiveArtifacts allowEmptyArchive: true, artifacts: '**/*.tfstate', followSymlinks: false
-                            echo "Infrastructure is Provisioned!"
+        stage('Docker Build/Publish') {
+            parallel {
+                stage('ems-api') {
+                    steps {
+                        script {
+                            try {
+                                sh 'cp -r /apps/ems-api/* .'
+                                def IMAGE_NAME = 'osiems'
+                                def ECR_REPOSITORY = 'osiems'
+                                // Build the Docker image
+                                sh "docker build -t ${IMAGE_NAME} ."
+                                // Tag the image with ECR repository URI
+                                sh "docker tag ${IMAGE_NAME}:latest ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${ECR_REPOSITORY}:${BUILD_NUMBER}"
+                                // Push the image to ECR
+                                sh "docker push ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${ECR_REPOSITORY}:${BUILD_NUMBER}"
+                            } catch (Exception e) {
+                                currentBuild.result = 'FAILURE'
+                                error("Docker image build/push failed: ${e.getMessage()}")
+                            }
+                        }
+                    }
+                }
+                stage('tsm-api') {
+                    steps {
+                        script {
+                            try {
+                                sh 'cp -r /apps/tsm-api/* .'
+                                def IMAGE_NAME = 'ositsm'
+                                def ECR_REPOSITORY = 'ositsm'
+                                // Build the Docker image
+                                sh "docker build -t ${IMAGE_NAME} ."
+                                // Tag the image with ECR repository URI
+                                sh "docker tag ${IMAGE_NAME}:latest ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${ECR_REPOSITORY}:${BUILD_NUMBER}"
+                                // Push the image to ECR
+                                sh "docker push ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${ECR_REPOSITORY}:${BUILD_NUMBER}"
+                            } catch (Exception e) {
+                                currentBuild.result = 'FAILURE'
+                                error("Docker image build/push failed: ${e.getMessage()}")
+                            }
                         }
                     }
                 }
